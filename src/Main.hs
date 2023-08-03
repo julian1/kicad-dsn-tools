f3756cc588abe69fd565da67216e085f0ccfc6fe
julian1 <git@julian1.io>
2023-08-03 20:58:36 +1000

    update

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,26 @@
+
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+
+import Data.Attoparsec.Text
+import System.IO
+import Data.Either
+import Data.Text
+import Lib
+import ExprParser
+
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =  do
+
+  putStrLn "Hello, Haskell!"
+
+  hFlush stdout
+  ls <- getLine
+
+  let stmtParseResult = parseOnly exprParser (pack ls)
+
+  putStrLn "whoot"
