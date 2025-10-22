c91b01956a63f3290ba0f7d93209b2503b20f517
julian1 <git@julian1.io>
2025-10-23 08:55:19 +1100

    squash update
    update
    
    update
    
    tidy

--- src/Main07.hs
+++ src/Main07.hs
@@ -3,9 +3,7 @@
 {-# LANGUAGE QuasiQuotes,  OverloadedStrings #-}
 
 {-
-  - usage
-    -  parse and write output expr for kicad_sch, kicad_pcb files
-    - takes single file argument of dsn file.
+  strip comments from a kicad s-exp file.  ie. schematic
 -}
 
 
@@ -22,12 +20,8 @@ import Lib
 
 -- import Text.RawString.QQ
 
---import Data.Text as T
 import Data.Text.IO as T(readFile, putStrLn, hPutStrLn)
-
-
 import Data.Text as T
-
 
 import System.IO(stdout)
 import System.Environment (getArgs)
@@ -39,22 +33,25 @@ import ExprPrint(exprPrint)
 
 
 {-
-  the problem with using a map  rather than a fold, is that we cannot discard a child node expression.
+  the problem with map  instead of a fold, is that we cannot discard a child node expression.
 
-  the problem with a filter,  is that we dont just want to include/reject a child.  but also want need to transform.
+  the problem with filter,  is that perhaps we want more than binary include/reject a child node.
 
-  but we could actually handle these ok.
 
-    instead of Maybe.   why not use an empty list.  if have nothing.
+  instead of Maybe.   why not use an empty list.  if have nothing.
+  but it doesn't work well with the idea of cons. the head.
 
 -}
 
-trans8layer ::  Expr -> Maybe Expr
-trans8layer expr =
-  {-
-      (text ...
-    -> (whoot ...
-  -}
+
+consMaybe :: Maybe a ->  [ a ] -> [ a ]
+consMaybe (Just x) xs  = x : xs
+consMaybe Nothing xs  = xs
+
+
+
+trans ::  Expr -> Maybe Expr
+trans expr =
 
   case expr of
     -- match node we want to work with
@@ -70,15 +67,44 @@ trans8layer expr =
     List xs
       -> Just $ List $ P.foldr f []  xs
       where
-        -- recursion and filter
-        f x xs =
-          case trans8layer x of
-            Just j -> j : xs
-            Nothing ->  xs
-
+        f x xs = consMaybe (trans x) xs
 
     -- catch all.
     _ -> Just expr
+
+
+
+
+main :: IO ()
+main =  do
+
+  args <- getArgs                  -- IO [String]
+
+  -- output filename
+  -- mapM P.putStrLn args
+
+  let file = P.head args
+
+  dsn <- T.readFile file
+
+
+  let exprParseResult = parseOnly exprParser dsn
+
+  either (\_ -> do
+      T.putStrLn $ "not a valid expr"
+    )
+    ( \expr -> do
+
+
+        let Just expr1   = trans expr
+
+
+        exprPrint stdout 0 expr1
+
+        T.hPutStrLn stdout "" -- newline
+
+    ) exprParseResult
+
 
 
 
@@ -103,43 +129,9 @@ ghci> foldr (:) [] [1..5]
   -- let transform = P.foldl  f  id (args_)
 
 
-
-main :: IO ()
-main =  do
-
-  args <- getArgs                  -- IO [String]
-
-  -- output filename
-  -- mapM P.putStrLn args
-
-  let file = P.head args
-
-  dsn <- T.readFile file
-
-
   {- eg. (or 1 1),    (+ 1 (+ 1 1 ))
       putStrLn "enter an expression!"
       hFlush stdout
       ls <- getLine
   -}
-
-  let exprParseResult = parseOnly exprParser dsn
-
-  either (\_ -> do
-      T.putStrLn $ "not a valid expr"
-    )
-    ( \expr -> do
-
-
-        let Just expr1   = trans8layer expr
-
-
-        exprPrint stdout 0 expr1
-
-        T.hPutStrLn stdout "" -- newline
-
-    ) exprParseResult
-
-
-
 
